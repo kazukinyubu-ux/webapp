c1b17877f911ad7484853bb7dc8f71e40e6328dd
kazukinyubu-ux <kazuki.nyubu@gmail.com>
2026-06-26 15:58:27 +0900

    Add files via upload

---
+++ photo-bingo/firebase.js
@@ -0,0 +1,10 @@
+// Firebase設定を入れる場所
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyDpx6PZzQJ_jGAc3FcLuvD3F3q_WDWpIZE",
+  authDomain: "photo-bingo-2ed9b.firebaseapp.com",
+  projectId: "photo-bingo-2ed9b",
+  storageBucket: "photo-bingo-2ed9b.firebasestorage.app",
+  messagingSenderId: "1067647665548",
+  appId: "1:1067647665548:web:a8132de69f0f882215c2a9"
+};
